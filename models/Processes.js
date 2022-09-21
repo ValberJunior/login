c275cceb0d10f2f269519533c6de2db224ff3bdd
ValberJunior <valberjr1992@gmail.com>
2022-09-21 01:34:05 -0300

    create processes models

---
+++ models/Processes.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ProcessSchema = mongoose.Schema({
+cpfpacient: { type: String, required: true, minlength: 11, maxlength: 17 },
+nprocess: { type: Number, required: true, minlength: 1, maxlength: 1000 },
+optionTerm: { type: String, required: true, minlength: 7, maxlength: 100 },
+lastAccess: { type: Date, required: true, minlength: 8, maxlength: 10 },
+status: { type: String, required: true, minlength: 5, maxlength: 100 },
+});
+
+module.exports = mongoose.model('Process', ProcessSchema, 'processes');
