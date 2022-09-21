64c02301599dc8b3dda70e076d84b5441fd225a8
ValberJunior <valberjr1992@gmail.com>
2022-09-21 01:34:17 -0300

    create processes router

---
+++ routes/processesRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const processController = require("../controllers/processesController");
+const router = express.Router();
+
+//get
+router.get("/processos", processController.all);
+
+router.get("/processos/:id", processController.getForParams);
+
+// patch and post 
+// router.patch("/processos", processController.update);
+router.post("/processos", processController.update);
+
+//post
+router.post("/novo", processController.create);
+
+//delete
+router.delete("/processos/:id", processController.delete);
+
+module.exports = router;
